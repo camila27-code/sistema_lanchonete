b29be7e46db99e35e63ba5807552be8e4d9ff832
Camila Lima <camila_lima62@outlook.com>
2026-03-22 11:08:31 -0300

    repositories.py

---
+++ memory.py
@@ -0,0 +1,10 @@
+from typing import Dict
+from domain.cliente import Cliente
+from domain.produto import Produto
+
+class MemoryDB:
+    def __init__(self):
+        self.clientes_por_cpf: Dict[str, Cliente] = {}
+        self.produtos_por_id: Dict[int, Produto] = {}
+
+db = MemoryDB()
