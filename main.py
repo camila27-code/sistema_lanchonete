3b37b8244abf931b6700a5058abe6b4476a70afa
Camila Lima <camila_lima62@outlook.com>
2026-05-14 23:15:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from api.routes.produtos import router as produto_router
 from fastapi.responses import JSONResponse
 from fastapi import Request
 
+from tortoise.contrib.fastapi import register_tortoise
+from infrastructure.tortoise.config import TORTOISE_ORM
+
 
 app = FastAPI(
                 title="Projeto Lanchonete",
@@ -29,5 +32,12 @@ app.include_router(clientes_router)
 app.include_router(produto_router)
 app.include_router(pedidos_router)
 
-# if __name__ == "__main__":
+register_tortoise(
+    app,
+    config=TORTOISE_ORM,
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+#if __name__ == "__main__":
 #    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
